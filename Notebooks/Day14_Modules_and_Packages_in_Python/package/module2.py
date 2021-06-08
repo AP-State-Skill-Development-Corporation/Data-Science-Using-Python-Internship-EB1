eaef4b97b48826b670e16d6de9a5255a95307386
Anil Kumar Teegala <34655912+AnilKumarTeegala@users.noreply.github.com>
2021-06-08 18:58:45 +0530

    Added day 1️⃣4️⃣ discussed content

---
+++ Notebooks/Day14_Modules_and_Packages_in_Python/package/module2.py
@@ -0,0 +1,25 @@
+"""Hey welcome to my module called module2 from package package"""
+
+def isEven(num):
+    """Check whether a number is even or not"""
+    if num % 2 == 0:
+        return True
+    return False
+
+
+def factorial(num):
+    """Generates factorial of a number"""
+    fact = 1
+    for i in range(1, num+1):
+        fact *= i
+    return fact
+
+pi = 3.14
+gravity = 9.9
+
+
+def isOdd(num):
+    """Check whether a number is odd or not"""
+    if num % 2 == 0:
+        return False
+    return True
